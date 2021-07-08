69cb623fd355337a7a686aa4ead3d1e51730f555
Anton Smarovydlo <anton.smarovydlo@spryker.com>
2021-07-08 15:22:38 +0300

    MP-4628: integrated merchant category + api feature.

---
+++ src/Orm/Zed/MerchantCategory/Persistence/SpyMerchantCategory.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Orm\Zed\MerchantCategory\Persistence;
+
+use Spryker\Zed\MerchantCategory\Persistence\Propel\AbstractSpyMerchantCategory as BaseSpyMerchantCategory;
+
+/**
+ * Skeleton subclass for representing a row from the 'spy_merchant_category' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ */
+class SpyMerchantCategory extends BaseSpyMerchantCategory
+{
+
+}
---
+++ src/Orm/Zed/MerchantCategory/Persistence/SpyMerchantCategoryQuery.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Orm\Zed\MerchantCategory\Persistence;
+
+use Spryker\Zed\MerchantCategory\Persistence\Propel\AbstractSpyMerchantCategoryQuery as BaseSpyMerchantCategoryQuery;
+
+/**
+ * Skeleton subclass for performing query and update operations on the 'spy_merchant_category' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ */
+class SpyMerchantCategoryQuery extends BaseSpyMerchantCategoryQuery
+{
+
+}
