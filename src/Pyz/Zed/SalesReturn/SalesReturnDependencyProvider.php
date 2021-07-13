cf731eae5722bee44d531496b521202a16e1e775
Anton Smarovydlo <anton.smarovydlo@spryker.com>
2021-07-13 10:36:58 +0300

    MP-4628: fixed marketplace return integration.

---
+++ src/Pyz/Zed/SalesReturn/SalesReturnDependencyProvider.php
@@ -0,0 +1,42 @@
+<?php
+
+
+namespace Pyz\Zed\SalesReturn;
+
+use Spryker\Zed\MerchantSalesReturn\Communication\Plugin\MerchantReturnCreateRequestValidatorPlugin;
+use Spryker\Zed\MerchantSalesReturn\Communication\Plugin\SalesReturn\MerchantReturnExpanderPlugin;
+use Spryker\Zed\MerchantSalesReturn\Communication\Plugin\SalesReturn\MerchantReturnPreCreatePlugin;
+use Spryker\Zed\SalesReturn\SalesReturnDependencyProvider as SprykerSalesReturnDependencyProvider;
+
+class SalesReturnDependencyProvider extends SprykerSalesReturnDependencyProvider
+{
+    /**
+     * @return \Spryker\Zed\SalesReturnExtension\Dependency\Plugin\ReturnPreCreatePluginInterface[]
+     */
+    protected function getReturnPreCreatePlugins(): array
+    {
+        return [
+            new MerchantReturnPreCreatePlugin(),
+        ];
+    }
+
+    /**
+     * @return \Spryker\Zed\SalesReturnExtension\Dependency\Plugin\ReturnCreateRequestValidatorPluginInterface[]
+     */
+    protected function getReturnCreateRequestValidatorPlugins(): array
+    {
+        return [
+            new MerchantReturnCreateRequestValidatorPlugin(),
+        ];
+    }
+
+    /**
+     * @return \Spryker\Zed\SalesReturnExtension\Dependency\Plugin\ReturnExpanderPluginInterface[]
+     */
+    protected function getReturnExpanderPlugins(): array
+    {
+        return [
+            new MerchantReturnExpanderPlugin(),
+        ];
+    }
+}
