64e15469875ebc5415b4fef29b759cbc49e7fd43
tobias.rosenstock <tobi.rosenstock@gmail.com>
2021-08-23 10:28:50 +0200

    MP-2783: fix after code review

--- src/Pyz/Zed/PriceCartConnector/PriceCartConnectorConfig.php
+++ src/Pyz/Zed/PriceCartConnector/PriceCartConnectorConfig.php
@@ -15,7 +15,7 @@ class PriceCartConnectorConfig extends SprykerPriceCartConnectorConfig
     /**
      * @return string[]
      */
-    public function getItemFieldsForIsSameItemComparison()
+    public function getItemFieldsForIsSameItemComparison(): array
     {
         return array_merge(parent::getItemFieldsForIsSameItemComparison(), [
             ItemTransfer::MERCHANT_REFERENCE,
