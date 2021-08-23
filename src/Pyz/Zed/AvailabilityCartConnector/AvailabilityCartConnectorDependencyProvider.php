369e7afff3a08d37b4fd0f58b5fc7f2f37386675
ilyakubanov <ilya.kubanov@spryker.com>
2021-08-23 16:58:54 +0300

    MP-5677, MP-5692: integrated MP-5630 and MP-1985

---
+++ src/Pyz/Zed/AvailabilityCartConnector/AvailabilityCartConnectorDependencyProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * This file is part of the Spryker Commerce OS.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace Pyz\Zed\AvailabilityCartConnector;
+
+use Spryker\Zed\AvailabilityCartConnector\AvailabilityCartConnectorDependencyProvider as SprykerAbstractBundleDependencyProvider;
+use Spryker\Zed\ProductOffer\Communication\Plugin\Cart\ProductOfferCartItemQuantityCounterStrategyPlugin;
+
+class AvailabilityCartConnectorDependencyProvider extends SprykerAbstractBundleDependencyProvider
+{
+    /**
+     * @return \Spryker\Zed\AvailabilityCartConnectorExtension\Dependency\Plugin\CartItemQuantityCounterStrategyPluginInterface[]
+     */
+    public function getCartItemQuantityCounterStrategyPlugins(): array
+    {
+        return [
+            new ProductOfferCartItemQuantityCounterStrategyPlugin(),
+        ];
+    }
+}
