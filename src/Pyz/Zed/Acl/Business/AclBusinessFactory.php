cca090410adbdb9ddbde2607a65799171985ed49
Nikita Peretyatko <peretyatko@logema.org>
2022-03-31 16:42:03 +0300

    MP-6691: fix to allow merchant users with root access login in BackOffice

---
+++ src/Pyz/Zed/Acl/Business/AclBusinessFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * This file is part of the Spryker Commerce OS.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace Pyz\Zed\Acl\Business;
+
+use Pyz\Zed\Acl\Business\Acl\AclConfigReader;
+use Spryker\Zed\Acl\Business\Acl\AclConfigReaderInterface;
+use Spryker\Zed\Acl\Business\AclBusinessFactory as SprykerAclBusinessFactory;
+
+class AclBusinessFactory extends SprykerAclBusinessFactory
+{
+    /**
+     * @return \Spryker\Zed\Acl\Business\Acl\AclConfigReaderInterface
+     */
+    public function createAclConfigReader(): AclConfigReaderInterface
+    {
+        return new AclConfigReader($this->getConfig());
+    }
+}
