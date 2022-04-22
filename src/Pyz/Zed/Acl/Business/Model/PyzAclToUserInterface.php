5b9ff4d4aba442b92523984ff3a482985b78c326
Ilya Antipov <antipov.i-dev@adict.ru>
2022-04-22 13:35:07 +0300

    Fix Travis

---
+++ src/Pyz/Zed/Acl/Business/Model/PyzAclToUserInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * This file is part of the Spryker Commerce OS.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace Pyz\Zed\Acl\Business\Model;
+
+use Spryker\Zed\Acl\Dependency\Facade\AclToUserInterface;
+
+interface PyzAclToUserInterface extends AclToUserInterface
+{
+}
