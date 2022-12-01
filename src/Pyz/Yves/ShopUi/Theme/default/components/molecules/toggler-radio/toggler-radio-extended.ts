f925ac20ef48de36e81c81ee19378ba531e5fa9d
Andrey Ilin <andrey.ilin@spryker.com>
2022-12-01 10:26:31 +0400

    CC-23837: Rvert a `readyCallback` method in toggler-radio-extended.

--- src/Pyz/Yves/ShopUi/Theme/default/components/molecules/toggler-radio/toggler-radio-extended.ts
+++ src/Pyz/Yves/ShopUi/Theme/default/components/molecules/toggler-radio/toggler-radio-extended.ts
@@ -1,6 +1,8 @@
 import TogglerRadio from 'ShopUi/components/molecules/toggler-radio/toggler-radio';
 
 export default class TogglerRadioExtended extends TogglerRadio {
+    protected readyCallback(): void {}
+
     toggle(addClass: boolean = this.addClass): void {
         this.targets.forEach((element: HTMLElement) => {
             if (!addClass) {
