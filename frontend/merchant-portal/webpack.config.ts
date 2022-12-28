282c272945f7a459a4ae0567b0dee2c18db7d8cc
danil.moroz <danil.moroz@spryker.com>
2022-12-28 15:09:40 +0200

    СС-25014: Integration - Webpack v5 & Angular v15.

--- frontend/merchant-portal/webpack.config.ts
+++ frontend/merchant-portal/webpack.config.ts
@@ -1,17 +1,14 @@
-import {
-    CustomWebpackBrowserSchema,
-    TargetOptions,
-} from "@angular-builders/custom-webpack";
-import * as webpack from "webpack";
-
-import { getMPEntryPointsMap } from "./entry-points";
+import { CustomWebpackBrowserSchema, TargetOptions } from '@angular-builders/custom-webpack';
+import * as webpack from 'webpack';
+import * as path from 'path';
+import { getMPEntryPointsMap } from './entry-points';
 
 export default async (
     config: webpack.Configuration,
     options: CustomWebpackBrowserSchema,
-    targetOptions: TargetOptions
+    targetOptions: TargetOptions,
 ): Promise<webpack.Configuration> => {
-    console.log("Resolving entry points...");
+    console.log('Resolving entry points...');
 
     const entryPointsMap = await getMPEntryPointsMap();
 
@@ -20,6 +17,11 @@ export default async (
     config.entry = {
         ...(config.entry as any),
         ...entryPointsMap,
+    };
+
+    config.resolve.alias = {
+        '~@spryker': path.resolve(__dirname, '../../node_modules/@spryker'),
+        '~@angular': path.resolve(__dirname, '../../node_modules/@angular'),
     };
 
     return config;
