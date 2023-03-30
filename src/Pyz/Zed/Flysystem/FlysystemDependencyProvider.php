4c3b29c3f96573d5e420b03bcaf18347fae6060a
Eugenia Poidenko <eugenia.poidenko@spryker.com>
2023-03-30 12:01:11 +0300

    SDK-1815 clean up the DP methods overrides

--- src/Pyz/Zed/Flysystem/FlysystemDependencyProvider.php
+++ src/Pyz/Zed/Flysystem/FlysystemDependencyProvider.php
@@ -10,25 +10,9 @@ namespace Pyz\Zed\Flysystem;
 use Spryker\Service\Flysystem\FlysystemDependencyProvider as SprykerFlysystemDependencyProvider;
 use Spryker\Service\FlysystemFtpFileSystem\Plugin\Flysystem\FtpFilesystemBuilderPlugin;
 use Spryker\Service\FlysystemLocalFileSystem\Plugin\Flysystem\LocalFilesystemBuilderPlugin;
-use Spryker\Service\Kernel\Container as SprykerContainer;
-use Spryker\Zed\Kernel\Container;
 
 class FlysystemDependencyProvider extends SprykerFlysystemDependencyProvider
 {
-    /**
-     * @param \Spryker\Service\Kernel\Container $container
-     *
-     * @return \Spryker\Service\Kernel\Container
-     */
-    protected function addFilesystemBuilderPluginCollection($container): SprykerContainer
-    {
-        $container->set(static::PLUGIN_COLLECTION_FILESYSTEM_BUILDER, function (Container $container) {
-            return $this->getFilesystemBuilderPluginCollection();
-        });
-
-        return $container;
-    }
-
     /**
      * @return array<\Spryker\Service\Flysystem\Dependency\Plugin\FlysystemFilesystemBuilderPluginInterface>
      */
