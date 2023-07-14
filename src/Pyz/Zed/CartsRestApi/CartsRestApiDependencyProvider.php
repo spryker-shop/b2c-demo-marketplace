43e3c9a9d801df268b7bdac75916f21e91685060
Ievgen Malykhin <ievgen.malykhin@spryker.com>
2023-07-14 16:34:10 +0300

    CC-29892: Fixed payment methods for checkout-data endpoint

--- src/Pyz/Zed/CartsRestApi/CartsRestApiDependencyProvider.php
+++ src/Pyz/Zed/CartsRestApi/CartsRestApiDependencyProvider.php
@@ -11,6 +11,7 @@ use Spryker\Zed\CartsRestApi\CartsRestApiDependencyProvider as SprykerCartsRestA
 use Spryker\Zed\CartsRestApi\Communication\Plugin\CartsRestApi\QuoteCreatorPlugin;
 use Spryker\Zed\CartsRestApiExtension\Dependency\Plugin\QuoteCreatorPluginInterface;
 use Spryker\Zed\DiscountPromotionsRestApi\Communication\Plugin\CartsRestApi\DiscountPromotionCartItemMapperPlugin;
+use Spryker\Zed\MerchantProductOffersRestApi\Communication\Plugin\CartsRestApi\MerchantProductOfferCartItemMapperPlugin;
 use Spryker\Zed\ProductBundleCartsRestApi\Communication\Plugin\BundleItemQuoteItemReadValidatorPlugin;
 use Spryker\Zed\ProductConfigurationsRestApi\Communication\Plugin\CartsRestApi\ProductConfigurationCartItemMapperPlugin;
 use Spryker\Zed\ProductOptionsRestApi\Communication\Plugin\CartsRestApi\ProductOptionCartItemMapperPlugin;
@@ -34,6 +35,7 @@ class CartsRestApiDependencyProvider extends SprykerCartsRestApiDependencyProvid
         return [
             new ProductOptionCartItemMapperPlugin(),
             new DiscountPromotionCartItemMapperPlugin(),
+            new MerchantProductOfferCartItemMapperPlugin(),
             new ProductConfigurationCartItemMapperPlugin(),
         ];
     }
