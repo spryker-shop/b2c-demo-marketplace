5252215586ba52d41bc0bcb137141c9a21b7aa6b
Mads Nielsen <mads.nielsen@spryker.com>
2023-10-02 14:03:25 +0200

    PBC-1677 Integrate TaxApp for Vertex 1.0

---
+++ src/Orm/Zed/TaxApp/Persistence/SpyTaxAppConfig.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Orm\Zed\TaxApp\Persistence;
+
+use Spryker\Zed\TaxApp\Persistence\Propel\AbstractSpyTaxAppConfig as BaseSpyTaxAppConfig;
+
+/**
+ * Skeleton subclass for representing a row from the 'spy_tax_app_config' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ */
+class SpyTaxAppConfig extends BaseSpyTaxAppConfig
+{
+
+}
---
+++ src/Orm/Zed/TaxApp/Persistence/SpyTaxAppConfigQuery.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Orm\Zed\TaxApp\Persistence;
+
+use Spryker\Zed\TaxApp\Persistence\Propel\AbstractSpyTaxAppConfigQuery as BaseSpyTaxAppConfigQuery;
+
+/**
+ * Skeleton subclass for performing query and update operations on the 'spy_tax_app_config' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ */
+class SpyTaxAppConfigQuery extends BaseSpyTaxAppConfigQuery
+{
+
+}
