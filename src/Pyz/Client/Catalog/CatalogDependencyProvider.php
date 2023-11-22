bd9c3e27db5477059f1291af47b0dd9c8d6affc2
Dmitriy Aseev <dmiseev@gmail.com>
2023-11-22 16:00:59 +0300

    Fixed stan issue.

--- src/Pyz/Client/Catalog/CatalogDependencyProvider.php
+++ src/Pyz/Client/Catalog/CatalogDependencyProvider.php
@@ -136,6 +136,7 @@ class CatalogDependencyProvider extends SprykerCatalogDependencyProvider
      */
     protected function createCatalogSearchResultFormatterPlugins(): array
     {
+        /** @phpstan-var \Spryker\Client\SearchExtension\Dependency\Plugin\ResultFormatterPluginInterface $rawCatalogSearchResultFormatterPlugin */
         $rawCatalogSearchResultFormatterPlugin = new RawCatalogSearchResultFormatterPlugin();
 
         return [
