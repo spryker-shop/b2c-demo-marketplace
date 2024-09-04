6f2c198968a5ffb1258aa1387af642f77455c123
Dmytro Asieiev <dmiseev@gmail.com>
2024-09-04 14:32:44 +0300

    CC-13685: Adjusted configuration.

--- src/Pyz/Yves/CatalogPage/CatalogPageConfig.php
+++ src/Pyz/Yves/CatalogPage/CatalogPageConfig.php
@@ -12,11 +12,6 @@ use SprykerShop\Yves\CatalogPage\CatalogPageConfig as SprykerCatalogPageConfig;
 class CatalogPageConfig extends SprykerCatalogPageConfig
 {
     /**
-     * @var bool
-     */
-    protected const IS_MINI_CART_ASYNC_MODE_ENABLED = true;
-
-    /**
      * @return bool
      */
     public function isVisibleEmptyRangeFilters(): bool
